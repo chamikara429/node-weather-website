3f0ba07152d6cfd3e283daef76cc86b416fcb1c4
Madusanka Hettiarachchi <madusanka@Madusankas-MacBook-Air.local>
2024-12-29 14:03:04 +1100

    set up app for hosting server - Render
    author email changed

--- src/app.js
+++ src/app.js
@@ -14,6 +14,8 @@ const __dirname = path.dirname(__filename)
 // console.log(path.join(__dirname, '../public'))
 
 const app = express()
+const port = process.env.PORT || 3000
+
 
 //Define paths for Express config
 const publicDirPath = path.join(__dirname, '../public')
@@ -122,6 +124,6 @@ app.get('*c', (req, res) => {
     })
 })
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
